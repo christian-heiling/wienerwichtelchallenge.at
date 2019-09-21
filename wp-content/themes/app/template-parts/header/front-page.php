7db18347dcd07ec95825593f0a2f564302b7316f
Christian Heiling <christian.heiling@yahoo.com>
2019-09-21 14:01:10 +0200

    * update layout more corresponding of given layout

--- wp-content/themes/app/template-parts/header/front-page.php
+++ wp-content/themes/app/template-parts/header/front-page.php
@@ -12,8 +12,6 @@
 <h1 class="home-title">Ganz Wien wichtelt!</h1>
 <p class="home-teaser">Über unsere Online-Wunschliste kannst du die Weihnachtswünsche von sozial bedürftigen Menschen erfüllen. </p>
 
-<iframe class="home-video" width="560" height="315" src="https://www.youtube.com/embed/L2fBO4y9wYc?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
 <div class="home-cta wp-block-button">
 <a href="/als-soziale-einrichtung-teilnehmen/" class="wp-block-button__link" style="margin: 0 auto;  display: inline-block;">Jetzt als Einrichtung teilnehmen</a>
 </div>
