e442108d73eafb206e9a15fdaad2617f90438c74
Christian Heiling <christian.heiling@yahoo.com>
2019-11-19 23:46:42 +0100

    * deactivate map

--- wp-content/themes/app/archive-social_organisation.php
+++ wp-content/themes/app/archive-social_organisation.php
@@ -16,10 +16,48 @@ get_header();
 $controller = \app\App::getInstance()->getController(get_post_type());
 ?>
 
+<?php
+$map = app\App::getInstance()->getOptions()->get('map');
+?>
+<script>
+    jQuery(document).ready(function () {
+        var mymap = L.map('mapid').setView<?php echo '([' . $map['latitude'] . ', ' . $map['longitude'] . '], ' . $map['zoom'] . ');'; ?>
+
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+            zoom: 2,
+            scrollWheelZoom: false,
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: 'mapbox.streets'
+        }).addTo(mymap);
+
+<?php
+foreach ($controller->getAll() as $organisation) {
+    $map = rwmb_get_value('map', [], $organisation->ID);
+
+    $popupText = '<strong>' . $organisation->post_title . '</strong><br>'
+            . 'Handlungsfeld: ' . rwmb_get_value('field_of_action', [], $organisation->ID) . '<br>'
+            . '<a href=\\"' . get_permalink($organisation) . '\\">Mehr erfahren</a>';
+
+    echo 'L.marker([' . $map['latitude'] . ', ' . $map['longitude'] . ']).addTo(mymap).bindPopup("' . $popupText . '");';
+}
+?>
+    });
+</script>
+
+
+
+
 <section id="primary" class="content-area event-archive">
     <main id="main" class="site-main">
 
 <?php if (have_posts()) : ?>
+
+            <article class="hentry entry">
+                <header class="entry-header">
+                    <h2 class="entry-title">Einrichtungen</h2>
+                </header>
 
 
     <?php
