9fc6e7d7528b990153073fa459629440b7635bc9
Christian Heiling <christian.heiling@yahoo.com>
2019-11-30 12:28:48 +0100

    * update plugins

--- wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
+++ wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit017eb8f7f4f826e85ae9e19efdadf08b::getLoader();
+return ComposerAutoloaderInit289345e5f665f8ee5e6f37aaf0c3ec22::getLoader();
